015d3cbbcbeec4943c60a14b98bfde7f0789efce
Abhishek Rathore <z.abhirathore@gmail.com>
2024-03-08 17:39:06 +0530

    basic non-winning tic-tac-toe completed

---
+++ src/Board.js
@@ -0,0 +1,39 @@
+import {useState} from 'react';
+
+
+function Square(props){
+  return <button className="square" onClick={props.onSquareClick}>{props.value}</button>
+}
+
+
+export default function Board(){
+  const [xIsNext, setXIsNext] = useState(true);
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleClick(i){
+    let squaresCopy = squares.slice();
+    squaresCopy[i] = xIsNext ? 'X' : 'O';
+    setXIsNext(!xIsNext);
+    setSquares(squaresCopy);
+  }
+
+  return (
+    <div className="board">
+      <div className="board-row">
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)}></Square>
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)}></Square>
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)}></Square>
+      </div>
+      <div className="board-row">
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)}></Square>
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)}></Square>
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)}></Square>
+      </div>
+      <div className="board-row">
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)}></Square>
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)}></Square>
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)}></Square>
+      </div>
+    </div>
+  );
+}
